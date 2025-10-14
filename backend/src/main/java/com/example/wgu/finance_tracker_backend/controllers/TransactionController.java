7dcd42b75e97b229c93cddf4a91dda5c1ae281d4
Brandon Beach <bbeac29@wgu.edu>
2025-10-14 12:43:43 -0400

    Updated the transaction controller to accept the authorized user(Principal) to pass the username of the user.

--- backend/src/main/java/com/example/wgu/finance_tracker_backend/controllers/TransactionController.java
+++ backend/src/main/java/com/example/wgu/finance_tracker_backend/controllers/TransactionController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,33 +24,38 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
+
     @PostMapping
-    public ResponseEntity<TransactionResponse> createTransaction(@RequestBody TransactionRequest transactionRequest) {
-        TransactionResponse transactionResponse = transactionService.createTransaction(transactionRequest);
+    public ResponseEntity<TransactionResponse> createTransaction(@RequestBody TransactionRequest transactionRequest, Principal principal) {
+        String username = principal.getName();
+        TransactionResponse transactionResponse = transactionService.createTransaction(transactionRequest, username);
         return new ResponseEntity<>(transactionResponse, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<TransactionResponse> updateTransaction(@PathVariable Long id, @RequestBody TransactionRequest transactionRequest) {
-        TransactionResponse transactionResponse = transactionService.updateTransaction(id, transactionRequest);
+    public ResponseEntity<TransactionResponse> updateTransaction(@PathVariable Long id, @RequestBody TransactionRequest transactionRequest, Principal principal) {
+        String username = principal.getName();
+        TransactionResponse transactionResponse = transactionService.updateTransaction(id, transactionRequest, username);
         return new ResponseEntity<>(transactionResponse, HttpStatus.OK);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<Void> deleteTransaction(@PathVariable Long id) {
-        transactionService.deleteTransaction(id);
+    public ResponseEntity<Void> deleteTransaction(@PathVariable Long id, Principal principal) {
+        String username = principal.getName();
+        transactionService.deleteTransaction(id, username);
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<TransactionResponse> getTransactionById(@PathVariable Long id) {
-        Optional<TransactionResponse> transactionResponse = transactionService.getById(id);
-        return transactionResponse.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
+//    @GetMapping("/{id}")
+//    public ResponseEntity<TransactionResponse> getTransactionById(@PathVariable Long id) {
+//        Optional<TransactionResponse> transactionResponse = transactionService.getById(id);
+//        return transactionResponse.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+//    }
 
     @GetMapping
-    public ResponseEntity<List<TransactionResponse>> getTransactionByAccountId(@RequestParam("accountId") Long accountId) {
-        List<TransactionResponse> transactionResponses = transactionService.getTransactionsByAccountId(accountId);
+    public ResponseEntity<List<TransactionResponse>> getTransactionByAccountId(@RequestParam("accountId") Long accountId, Principal principal) {
+        String username = principal.getName();
+        List<TransactionResponse> transactionResponses = transactionService.getTransactionsByAccountId(accountId, username);
         return ResponseEntity.ok(transactionResponses);
     }
 }
